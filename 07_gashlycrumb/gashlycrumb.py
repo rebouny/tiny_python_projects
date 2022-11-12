b17f7b634ae3dc60647566d7aa7866444a9eca86
Martin Schuh <development@rebouny.net>
2022-11-12 23:19:52 +0100

    added solution for 07

---
+++ 07_gashlycrumb/gashlycrumb.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+Author : Martin Schuh <development@rebouny.net>
+Date   : 2022-11-12
+Purpose: Looking items up in a dictionary
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Gashlycrumb',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('letter',
+                        metavar='letter',
+                        nargs='+',
+                        help='Letter(s)')
+
+    parser.add_argument('-f',
+                        '--file',
+                        help='Input file',
+                        metavar='FILE',
+                        type=argparse.FileType('rt'),
+                        default='gashlycrumb.txt')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+
+    lookup = {line[0].lower(): line.rstrip() for line in args.file}
+
+    for letter in args.letter:
+        if letter.lower() in lookup:
+            print(f"{lookup[letter.lower()]}")
+        else:
+            print(f'I do not know "{letter}".')
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
